c3c882336171cebcefa5f31172c2512ade947ed1
VISHNU MARAGANI <VISHNU.MARAGANI@conduent.com>
2021-06-15 15:21:47 +0530

    add function.

--- CodeQLConsoleAPP/CodeQLConsoleAPP/Program.cs
+++ CodeQLConsoleAPP/CodeQLConsoleAPP/Program.cs
@@ -5,12 +5,25 @@ namespace CodeQLConsoleAPP
     class Program
     {
 
-
         public static string value = "";
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void AddTwoNumbers(int a, int b)
+        {
+
+            try
+            {
+                a = a + b;
+            }
+            catch (Exception ex)
+            {
+                //throw;
+            }
+        }
+
     }
 }

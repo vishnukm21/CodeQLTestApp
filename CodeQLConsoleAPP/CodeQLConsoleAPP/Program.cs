bbca81a8f977864c8afa36c2ed5523ee85b56690
VISHNU MARAGANI <VISHNU.MARAGANI@conduent.com>
2021-06-15 15:13:12 +0530

    updated

--- CodeQLConsoleAPP/CodeQLConsoleAPP/Program.cs
+++ CodeQLConsoleAPP/CodeQLConsoleAPP/Program.cs
@@ -4,6 +4,10 @@ namespace CodeQLConsoleAPP
 {
     class Program
     {
+
+
+        public static string value = "";
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
